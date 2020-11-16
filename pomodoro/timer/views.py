1b5b01cb84c3d83fdf03e0736f1672d14ec06804
heavysugar <44726789+heavysugar@users.noreply.github.com>
2020-11-16 13:02:15 -0500

    added simple text http responses to 2 endpoints

--- pomodoro/timer/views.py
+++ pomodoro/timer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(response):
+    return HttpResponse ("<h1>Welcome to Pomodoro timer!</h1>")
+
+def v1(response):
+    return HttpResponse ("<h1>This is v1!</h1>")
