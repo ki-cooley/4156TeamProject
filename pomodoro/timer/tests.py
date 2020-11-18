86150866eb0cfd3b35229be6a52c9e833b21f586
ki-cooley <nhc2116@columbia.edu>
2020-11-17 23:49:59 -0500

    Implement local browser history tracker

--- pomodoro/timer/tests.py
+++ pomodoro/timer/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+# from django.test import TestCase
+import os
+from timer import browserhistory
+from unittest import TestCase
 
-# Create your tests here.
+class BrowserHistoryTestLinux(TestCase):
+    def test_get_database_path(self):
+        linux_path = browserhistory.get_database_paths()['chrome']
+        true_path = os.path.expanduser('~') + "/.config/google-chrome/Default/History"
+        self.assertEqual(linux_path, true_path)
+
+    def test_get_history(self):
+        history = browserhistory.get_browserhistory()['chrome']
+        self.assertTrue(history)
