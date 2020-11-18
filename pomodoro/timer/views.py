c524f6dd66f380c332cc56e80aeab249c349f96d
heavysugar <44726789+heavysugar@users.noreply.github.com>
2020-11-18 14:23:02 -0500

    Linked pomodoro-timer.py to front end button described in home.html and added entries to DB

--- pomodoro/timer/views.py
+++ pomodoro/timer/views.py
@@ -1,20 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import requests as r
 import sys
 from subprocess import run, PIPE
+from django.views.generic import TemplateView 
+from . import pomodoro_timer as p
+# from p import run_timer
 
-# Create your views here.
 
 def index(response):
     return HttpResponse ("<h1>Welcome to Pomodoro timer!</h1>")
 
 def home(response):
-    return render(response, "main/home.html", {})
+    return render(response, "timer/home.html", {})
+
+def start(response):
+    return render(response, "timer/start.html", {})
 
 def output(response):
-    data=r.get("http://127.0.0.1:8800/")
-    # print(data.text)
-    data=data.text
-    return render(response, "main/home.html")
+    print (response.method)
+    if response.method == "POST":
+        print ("******")
+        print (response.POST)
+        print ("******")
+        # parsing Http Response from UI start button 
+        alldata = response.POST
+        start_button_value = alldata.get("start_timer", 0)
+        print("start_button_value : " + start_button_value)
+        # call timer function
+        pomodoro = p.Pomodoro()
+        pomodoro.run_timer()
+
+        
+        return render(response, "timer/home.html", {})
+    else: #GET method
+        redirect('start/', "timer/home.html", {})
+        return render(response, "timer/home.html", {})
+        # do 
+        
     
