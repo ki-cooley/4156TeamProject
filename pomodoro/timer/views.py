a5aea230512c605426ad9767762c36aa2d97ebc6
WYF11111111 <yw3471@columbia.edu>
2020-11-19 14:55:56 +0800

    adjustion to the timer folder

--- pomodoro/timer/views.py
+++ pomodoro/timer/views.py
@@ -3,24 +3,15 @@ from django.http import HttpResponse
 import requests as r
 import sys
 from subprocess import run, PIPE
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 from . import pomodoro_timer as p
 
 
-
 def index(response):
-    return HttpResponse ("<h1>Welcome to Pomodoro timer!</h1>")
+    return HttpResponse("<h1>Welcome to Pomodoro timer!</h1>")
 
 def home(response):
     return render(response, "timer/home.html", {})
-<<<<<<< HEAD
-
-def output(response):
-    data=r.get("http://127.0.0.1:8800/")
-    # print(data.text)
-    data=data.text
-    return render(response, "timer/home.html")
-=======
 
 def start(response):
     if response.method == "POST":
@@ -29,28 +20,31 @@ def start(response):
         reset_button_value = alldata.get("reset_timer", 0)
         print("reset_button_value : " + str(reset_button_value))
         return redirect('/start')
+
     else:
         pomodoro = p.Pomodoro()
         pomodoro.run_timer()
         return render(response, "timer/start.html", {})
 
 def output(response):
-    print (response.method)
+    print(response.method)
+
     if response.method == "POST":
-        print ("******")
-        print (response.POST)
-        print ("******")
-        # parsing Http Response from UI start button 
+        print("******")
+        print(response.POST)
+        print("******")
+
+        # parsing Http Response from UI start button
         alldata = response.POST
         start_button_value = alldata.get("start_timer", 0)
         print("start_button_value : " + start_button_value)
         # call timer function
         return redirect('/start/')
 
-    else: #GET method
+    else:  # GET method
         # redirect('start/', "timer/home.html", {})
         return render(response, "timer/home.html", {})
-        # do 
-        
->>>>>>> 0352219ddbf9ac6520333797808400b2ceeb02b8
-    
+        # do
+
+
+
