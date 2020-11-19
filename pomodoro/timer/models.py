4c4ff03540f9d1b6874e94f29c38a84db8e841ab
ki-cooley <nhc2116@columbia.edu>
2020-11-19 15:54:05 -0500

    Alter SessionActivity model to include user_id and remove session_id

--- pomodoro/timer/models.py
+++ pomodoro/timer/models.py
@@ -11,9 +11,10 @@ class Session(models.Model):
 
 class SessionActivity(models.Model):
     #id (primary key) is automatically added
-    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    # session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
     site_url = models.URLField()
     visit_timestamp = models.DateTimeField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class BlockedSite(models.Model):
     #id (primary key) is automatically added
