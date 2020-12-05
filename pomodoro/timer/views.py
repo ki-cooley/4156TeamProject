f0f6959abbd4c5fc1f9a902b4a143c4ebce86b0a
heavysugar <44726789+heavysugar@users.noreply.github.com>
2020-12-05 18:18:42 -0500

    added user as parameter to run_timer

--- pomodoro/timer/views.py
+++ pomodoro/timer/views.py
@@ -20,27 +20,24 @@ class SessionActivityViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
-# @login_required
 def start(response):
     """start view."""
     if response.method == "POST":
         alldata = response.POST
         reset_button_value = alldata.get("reset_timer", 0)
         skip_value = alldata.get("skip_to_break", 0)
+
         if str(reset_button_value) == "reset":
             return redirect('/start/')
         if str(skip_value) == "skip_to_break":
+            current_user = response.user
+            pomodoro = p.Pomodoro()
+            pomodoro.run_timer(current_user)
             return redirect('/break/')
     else:
-        alldata = response.POST
-        usernamefield = alldata.get("username", 0)
-        firstnamefield =alldata.get("firstname", 0)
-        lastnamefield = alldata.get("lastname", 0)
-        passwordfield = alldata.get("password1", 0)
-        emailfield = alldata.get("email", 0)
-
+        current_user = response.user
         pomodoro = p.Pomodoro()
-        pomodoro.run_timer()
+        pomodoro.run_timer(current_user)
         return render(response, "timer/start.html", {})
 
 
@@ -52,6 +49,9 @@ def start_break(response):
         reset_button_value = alldata.get("new_session", 0)
         print("new_session : " + str(reset_button_value))
         if str(reset_button_value) == "new_session":
+            current_user = response.user
+            pomodoro = p.Pomodoro()
+            pomodoro.run_timer(current_user)
             return redirect('/start/')
     else:
         return render(response, "timer/break.html", {})
@@ -59,10 +59,19 @@ def start_break(response):
 
 def home(response):
     """start endpoint."""
-    if response.method == "POST":
-        alldata = response.POST
-        start_button_value = alldata.get("start_timer", 0)
-        print("start_button_value : " + str(start_button_value))
-        # call timer function
-        return redirect('/start/')
-    return render(response, "timer/home.html", {})
+    if response.user.is_authenticated:
+
+        current_user = response.user
+        print( current_user.id)
+        print("USER ID ")
+        if response.method == "POST":
+            alldata = response.POST
+            start_button_value = alldata.get("start_timer", 0)
+            print("start_button_value : " + str(start_button_value))
+            # call timer function
+            return redirect('/start/')
+        return render(response, "timer/home.html", {})
+    else:
+        if response.method == "POST":
+            return redirect('/account/login/')
+        return render(response, "timer/home.html", {})
