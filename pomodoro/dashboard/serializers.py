a862c1d8cc218ca7cd885495c7f1d3b788cf98ff
hy2616 <hy2616@columbia.edu>
2020-12-06 14:57:16 -0500

    Dashboard update

--- pomodoro/dashboard/serializers.py
+++ pomodoro/dashboard/serializers.py
@@ -6,3 +6,21 @@ class DashboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDashboard
         fields = "__all__"
+
+class TimerBlockedsiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimerBlockedsite
+        fields = "__all__"
+
+
+class TimerSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimerSession
+        fields = "__all__"
+
+
+class TimerSessionactivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimerSessionactivity
+        fields = "__all__"
+
