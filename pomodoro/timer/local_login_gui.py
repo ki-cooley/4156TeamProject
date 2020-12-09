346bb24c96a33cdbf6d1031b36fba6be81d8348b
Yuanxin-Yang <55468947+Yuanxin-Yang@users.noreply.github.com>
2020-12-08 22:20:02 -0500

    Update local_login_gui.py

--- pomodoro/timer/local_login_gui.py
+++ pomodoro/timer/local_login_gui.py
@@ -1,24 +1,26 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')
-# All the stuff inside your window.
-layout = [  [sg.Text('Please login here')],
-            [sg.Text("User id"), sg.InputText()],
-            [sg.Text("Password"), sg.InputText(key='password', password_char='*')],
-            [sg.Button('Login'), sg.Button('Cancel')] ]
+def login_gui():
+    sg.theme('DarkAmber')   # Add a touch of color
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Please login here')],
+                [sg.Text("User id"), sg.InputText()],
+                [sg.Text("Password"), sg.InputText(key='password', password_char='*')],
+                [sg.Button('Login'), sg.Button('Cancel')] ]
 
-# Create the Window
-window = sg.Window('Login', layout)
+    # Create the Window
+    window = sg.Window('Login', layout)
 
-# Event Loop to keep reading until login info matches
-while True:
-    login = False
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user clicks cancel or close, window will close, no successful login
-        break
-    if event == 'Login': # if user clicks login and info matches, window will close, login successful
-        if values[1] and values[2]:
-            login = True
-            break
 
-window.close()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        window.close()
+
+    if event == 'Login':
+        token = login(values[1], values[2])
+        if token is None:
+            sg.popup("Login unsuccessful, please enter correct user id and password")
+            login_gui()
+        else:
+            window.close()
+
