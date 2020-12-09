e13eeef6d0fcd00f9708adbab57d05eb5f3748c3
Yuanxin-Yang <55468947+Yuanxin-Yang@users.noreply.github.com>
2020-12-08 20:20:17 -0500

    Update blocker.py

--- pomodoro/timer/blocker.py
+++ pomodoro/timer/blocker.py
@@ -1,50 +1,18 @@
+"""
+a simple website blocker worked by redirecting desired website's ip
+to ip "127.0.0.1" which is the localhost
+"""
+
 import time
 from datetime import datetime, timedelta
 import platform
+import os
+
+
 
 def blocker(values, block_time):
 
-    # sample websites that will be blocked
-    social_media = [
-        "www.facebook.com", "facebook.com",
-        "www.twitter.com", "twitter.com",
-        "www.whatsapp.com", "whatsapp.com",
-        "www.tumblr.com", "tumblr.com",
-        "www.reddit.com", "reddit.com"
-        "www.pinterest.com", "pinterest.com",
-        "www.sina.com", "sina.com"
-    ]
-
-    entertainment = [
-        "www.youtube.com", "youtube.com",
-        "www.tiktok.com", "tiktok.com",
-        "www.hulu.com", "hulu.com"
-        "www.steam.com", "steam.com",
-        "www.crunchyroll.com", "crunchyroll.com",
-        "www.douban.com", "douban.com"
-
-    ]
-
-    shopping = [
-        "www.amazon.com", "amazon.com",
-        "www.sephora.com", "sephora.com",
-        "www.asos.com", "asos.com",
-        "www.bestbuy.com", "bestbuy.com",
-        "www.macys.com", "macys.com",
-        "www.ebay.com", "ebay.com",
-        "www.zappos.com", "zappos.com"
-        "www.costco.com", "costco.com",
-        "www.bjs.com", "bjs.com"
-    ]
-
-    sites_to_block = []
-    if values[1]:
-        sites_to_block.extend(social_media)
-    if values[2]:
-        sites_to_block.extend(entertainment)
-    if values[3]:
-        sites_to_block.extend(shopping)
-
+    block_list_path = os.getcwd()
 
     Linux = "/etc/hosts"
     Windows = r"C:\Windows\System32\drivers\etc\hosts"
@@ -53,8 +21,66 @@ def blocker(values, block_time):
 
     if platform.system() == "Linux" or platform.system() == "Darwin":
         host_path = Linux
+        block_list_path  = block_list_path+"/block_list.txt"
     elif platform.system() == "Windows":
         host_path = Windows
+        block_list_path = block_list_path+r"\block_list.txt"
+
+    sites_to_block = []
+    if values[1]:
+        social_media = []
+        with open(block_list_path, "r") as lists:
+            list = lists.readlines()
+            sm_index = list.index("social_media:\n")
+            for i in range(sm_index, len(list)):
+                if list[i] == "\n":
+                    break
+                list[i] = list[i][:list[i].rfind("\n")]
+                if list[i] != "social_media:":
+                    social_media.append("www."+list[i]+".com")
+                    social_media.append(list[i]+".com")
+
+        sites_to_block.extend(social_media)
+    if values[2]:
+        entertainment = []
+        with open(block_list_path, "r") as lists:
+            list = lists.readlines()
+            ent_index = list.index("entertainment:\n")
+            for i in range(ent_index, len(list)):
+                if list[i] == "\n":
+                    break
+                list[i] = list[i][:list[i].rfind("\n")]
+                if list[i] != "entertainment:":
+                    entertainment.append("www." + list[i] + ".com")
+                    entertainment.append(list[i] + ".com")
+        sites_to_block.extend(entertainment)
+    if values[3]:
+        shopping = []
+        with open(block_list_path, "r") as lists:
+            list = lists.readlines()
+            shop_index = list.index("shopping:\n")
+            for i in range(shop_index, len(list)):
+                if list[i] == "\n":
+                    break
+                list[i] = list[i][:list[i].rfind("\n")]
+                if list[i] != "shopping:":
+                    shopping.append("www." + list[i] + ".com")
+                    shopping.append(list[i] + ".com")
+        sites_to_block.extend(shopping)
+
+
+    for i in range(4,9):
+        if values[i] is not None:
+            values[i].split(.)
+            sites_to_block.append("www." + values[i] + ".com")
+            sites_to_block.append(values[i]+".com")
+
+    for i in range(9, 14):
+        if values[i] is not None:
+            if "www." + values[i] + ".com" in sites_to_block:
+                sites_to_block.remove("www." + values[i] + ".com")
+                sites_to_block.remove(values[i] + ".com")
+
 
     current_time = datetime.now()
     block_time_sec = block_time * 60
@@ -81,3 +107,5 @@ def blocker(values, block_time):
                 hostfile.truncate()
             print('Blocking time is over. Good job!')
             blocker_status = False
+
+
